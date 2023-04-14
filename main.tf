b734551a1d08308f4ab247e1ed81d4c34d4ef5d2
Daniel Newman <daniel.newman@x-rd.com.au>
2023-04-14 17:13:08 +1000

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+provider "google" {
+  project = "angular-port-383706"
+  region  = "australia-southeast1"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "terraform-network"
+    access_config {
+    }
+  }
+}
