4b64a686de989f9e5d34249854cb0e35e9675d12
Daniel Newman <daniel.newman@x-rd.com.au>
2023-04-14 17:28:13 +1000

    self link

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network = "terraform-network"
+    network = google_compute_network.vpc_network.self_link
     access_config {
     }
   }
